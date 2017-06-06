1cb6e81fe0625e59c686a9be6bfc6cbd4a40e366
Martin Carolan <martin.carolan@itv.com>
2017-06-06 08:30:50 +0100

    Merged reduction + pricing logic and test cases

--- src/main/scala/net/mcarolan/hmrcshopping/CheckoutCalculator.scala
+++ src/main/scala/net/mcarolan/hmrcshopping/CheckoutCalculator.scala
@@ -16,22 +16,22 @@ case class Offer(product: Product, quantityRequired: Int, numberFree: Int)
 
 object CheckoutCalculator {
 
-  def price(basket: List[Product]): BigDecimal =
-    basket.foldLeft(BigDecimal(0)){ case (acc, product) =>
-      acc + product.unitPrice
-    }
-
-  def reduceBasketWithOffers(basket: List[Product], offers: List[Offer]): List[Product] = {
+  def priceWithOffers(basket: List[Product], offers: List[Offer]): BigDecimal = {
     val productQuantity: Map[Product, Int] = basket.groupBy(identity).mapValues(_.size)
 
-    productQuantity.flatMap { case (product, quantity) =>
-      val offerOpt = offers.find(_.product == product)
-      val numberFree = offerOpt.fold(0) { offer =>
-        quantity / offer.quantityRequired * offer.numberFree
-      }
+    val basketAfterOffersApplied =
+      productQuantity.flatMap { case (product, quantity) =>
+        val offerOpt = offers.find(_.product == product)
+        val numberFree = offerOpt.fold(0) { offer =>
+          quantity / offer.quantityRequired * offer.numberFree
+        }
 
-      List.fill(quantity + numberFree * -1)(product)
-    }.toList
+        List.fill(quantity + numberFree * -1)(product)
+      }.toList
+
+    basketAfterOffersApplied.foldLeft(BigDecimal(0)){ case (acc, product) =>
+      acc + product.unitPrice
+    }
   }
 
 }
--- src/test/scala/net/mcarolan/hmrcshopping/CheckoutCalculatorTest.scala
+++ src/test/scala/net/mcarolan/hmrcshopping/CheckoutCalculatorTest.scala
@@ -5,13 +5,13 @@ import org.scalatest._
 class CheckoutCalculatorTest extends FunSuite with Matchers {
 
   test("Price of an empty basket should be 0") {
-    CheckoutCalculator.price(List.empty) shouldBe BigDecimal(0)
+    CheckoutCalculator.priceWithOffers(List.empty, offers = List.empty) shouldBe BigDecimal(0)
   }
 
   test("Price of a basket with a single item should be item's unit price") {
     Set(Orange, Apple).foreach { fruit =>
       withClue(fruit) {
-        CheckoutCalculator.price(List(fruit)) shouldBe fruit.unitPrice
+        CheckoutCalculator.priceWithOffers(List(fruit), offers = List.empty) shouldBe fruit.unitPrice
       }
     }
   }
@@ -19,7 +19,7 @@ class CheckoutCalculatorTest extends FunSuite with Matchers {
   test("Price of basket with all items of one price should be unit price * basket size") {
     Set(Orange, Apple).foreach { fruit =>
       withClue(fruit) {
-        CheckoutCalculator.price(List.fill(5)(fruit)) shouldBe fruit.unitPrice * 5
+        CheckoutCalculator.priceWithOffers(List.fill(5)(fruit), offers = List.empty) shouldBe fruit.unitPrice * 5
       }
     }
   }
@@ -29,35 +29,35 @@ class CheckoutCalculatorTest extends FunSuite with Matchers {
     val numberOfApples = 4
     val basket = List.fill(numberOfOranges)(Orange) ++ List.fill(numberOfApples)(Apple)
     val expectedPrice = Orange.unitPrice * numberOfOranges + Apple.unitPrice * numberOfApples
-    CheckoutCalculator.price(basket) shouldBe expectedPrice
+    CheckoutCalculator.priceWithOffers(basket, offers = List.empty) shouldBe expectedPrice
   }
 
   val appleOffer = Offer(Apple, quantityRequired = 2, numberFree = 1)
   val orangeOffer = Offer(Orange, quantityRequired = 3, numberFree = 1)
 
-  test("Reduce basket with 2 Apples only and bogof offer yields 1 apple") {
-    CheckoutCalculator.reduceBasketWithOffers(List(Apple, Apple), List(appleOffer)) shouldBe List(Apple)
+  test("Price basket with 2 Apples only and bogof offer yields price of 1 apple") {
+    CheckoutCalculator.priceWithOffers(List(Apple, Apple), List(appleOffer)) shouldBe Apple.unitPrice
   }
 
-  test("Reduce basket with 3 Apples only and bogof offer yields 2 apples") {
-    CheckoutCalculator.reduceBasketWithOffers(List(Apple, Apple, Apple), List(appleOffer)) shouldBe List(Apple, Apple)
+  test("Price basket with 3 Apples only and bogof offer yields price of 2 apples") {
+    CheckoutCalculator.priceWithOffers(List(Apple, Apple, Apple), List(appleOffer)) shouldBe Apple.unitPrice * 2
   }
 
-  test("Reduce basket with 2 Apples and 1 Orange, with bogof for Apples yields 1 Apple 1 Orange") {
-    val result = CheckoutCalculator.reduceBasketWithOffers(List(Apple, Orange, Apple), List(appleOffer))
-    result should contain theSameElementsAs List(Apple, Orange)
+  test("Price basket with 2 Apples and 1 Orange, with bogof for Apples yields price of 1 Apple 1 Orange") {
+    val result = CheckoutCalculator.priceWithOffers(List(Apple, Orange, Apple), List(appleOffer))
+    result should be (Apple.unitPrice + Orange.unitPrice)
   }
 
-  test("Reduce basket with 2 Apples, 3 Oranges with bogof Apples, 3 for 2 Oranges yields 1 Apple 2 Oranges") {
+  test("Price basket with 2 Apples, 3 Oranges with bogof Apples, 3 for 2 Oranges yields price of 1 Apple 2 Oranges") {
     val basket = List(Apple, Orange, Apple, Orange, Orange)
-    val result = CheckoutCalculator.reduceBasketWithOffers(basket, List(appleOffer, orangeOffer))
-    result should contain theSameElementsAs List(Apple, Orange, Orange)
+    val result = CheckoutCalculator.priceWithOffers(basket, List(appleOffer, orangeOffer))
+    result should be (Apple.unitPrice + Orange.unitPrice * 2)
   }
 
-  test("Basket reduction with multiple applications of offers") {
+  test("Basket pricing with multiple applications of offers") {
     val basket = List(Apple, Orange, Apple, Orange, Apple, Orange, Orange, Apple, Apple)
-    val result = CheckoutCalculator.reduceBasketWithOffers(basket, List(appleOffer, orangeOffer))
-    result should contain theSameElementsAs List(Apple, Apple, Apple, Orange, Orange, Orange)
+    val result = CheckoutCalculator.priceWithOffers(basket, List(appleOffer, orangeOffer))
+    result should be (Apple.unitPrice * 3 + Orange.unitPrice * 3)
   }
 
 }
